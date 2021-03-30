281a59dd9ce89aeb7be6d00d5ad021a5c51f29c8
ShakirzyanovSH <shakirzyanov144@gmail.com>
2021-03-30 22:16:08 +0700

    first commit

---
+++ src/views/RickAndMortyList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+	  rick and morty 
+	  <div class="characters-list">
+		  <character-block 
+		  v-for="character in characters" 
+		  :key="character.id" 
+	      :character="character"
+	      />
+	  </div>  
+	  <paginate
+        v-model="currentPage"
+        :page-count="pages"
+        :page-range="3"
+        :margin-pages="2"
+        :click-handler="clickCallback"
+        :prev-text="'Prev'"
+        :next-text="'Next'"
+        :container-class="'pagination'"
+        :page-class="'page-item'">
+      </paginate>
+  </div>
+</template>
+
+<script>
+import CharacterBlock from '@/components/CharacterBlock.vue';
+import Paginate from 'vuejs-paginate'
+
+export default {
+  name: 'rick-and-morty-list',
+  components: {
+	  CharacterBlock,
+	  Paginate
+  },
+  data() {
+	  return {
+		  currentPage: 1,
+	  }
+  },
+  computed: {
+	  characters() {
+		  console.log(this.$store.getters['getCharactersByPage'](this.currentPage), 'characters')
+		  return this.$store.getters['getCharactersByPage'](this.currentPage);
+     },
+	 firstCharacter() {
+		 return this.$store.getters['getCharacterById']({id: 1, pdge: 1});
+	 },
+	 pages() {
+		 return this.$store.state.pages;
+	 }
+  },
+  watch: {
+      currentPage: {
+		  handler(page) {
+            this.$store.dispatch('fetchCharacters', page);
+		  },
+		  immediate: true
+	  },
+  },
+  methods: {
+	  clickCallback(pageNum) {
+      console.log({pageNum})
+    },
+  },
+}
+</script>
